690f09a14a15498946943ba6be9e3b04c82a136c
Matthew Phillips <matthew@matthewphillips.info>
2014-02-03 11:05:55 -0500

    Added CodeMirror

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,48 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var str2jsify = require('string-to-jsify');
 var traceur = require('gulp-traceur');
 
 gulp.task('scripts', function() {
   gulp.src('lib/fiddle.js')
     .pipe(traceur())
-    .pipe(browserify({debug: true}))
+    .pipe(browserify({
+      debug: true,
+      transform: [
+        'debowerify',
+        str2jsify.configure({extensions: '.mustache'})
+      ],
+      shim: {
+        'codemirror': {
+          path: 'bower_components/codemirror/lib/codemirror.js',
+          exports: 'CodeMirror',
+        },
+        'xmlmode': {
+          path: 'bower_components/codemirror/mode/xml/xml.js',
+          exports: 'CodeMirror'
+        },
+        'cssmode': {
+          path: 'bower_components/codemirror/mode/css/css.js',
+          exports: 'CodeMirror'
+        },
+        'jsmode': {
+          path: 'bower_components/codemirror/mode/javascript/javascript.js',
+          exports: 'CodeMirror'
+        },
+        'htmlmode': {
+          path: 'bower_components/codemirror/mode/htmlmixed/htmlmixed.js',
+          exports: 'CodeMirror'
+        }
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+  gulp.src(['bower_components/codemirror/lib/codemirror.css'])
+    .pipe(concat('fiddle.css'))
     .pipe(gulp.dest('dist'));
 });
 
